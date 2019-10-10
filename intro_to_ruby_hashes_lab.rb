881dd80254f10b403a0e1e82f29eb6951afded10
Alexis Chilinski <chilinski.a@gmail.com>
2019-10-10 16:44:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,11 +19,12 @@ def monopoly_with_third_tier
   # complexity.
   {:railroads => {
     :pieces => 4, :rent_in_dollars => {
-      :names => "",
+      :key1 => "",
       :key2 => "",
       :key3 => "",
       :key4 => ""
-      }
+      },
+    :names => ""
     }
   }
 end
